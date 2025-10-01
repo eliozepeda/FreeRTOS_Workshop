8541c9d8e1cdd556f5ffd0230095037407507863
OMEN-Elio\Elio <zepedasancheselio@gmail.com>
2025-10-01 13:39:45 -0600

    FreeRTOS Workshop Base Code 1.2

--- Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Users/Elio/Documents/Firmware\ Focus/RTOs/FreeRTOS_Course_FirmwareFocusAcademy/NUCLEO_F411RE_Freertos_Workspace/FreeRTOS_Workshop/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-C:/Users/Elio/Documents/Firmware\ Focus/RTOs/FreeRTOS_Course_FirmwareFocusAcademy/NUCLEO_F411RE_Freertos_Workspace/FreeRTOS_Workshop/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
--- Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
- ../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Users/Elio/Documents/Firmware\ Focus/RTOs/FreeRTOS_Course_FirmwareFocusAcademy/NUCLEO_F411RE_Freertos_Workspace/FreeRTOS_Workshop/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-../common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-C:/Users/Elio/Documents/Firmware\ Focus/RTOs/FreeRTOS_Course_FirmwareFocusAcademy/NUCLEO_F411RE_Freertos_Workspace/FreeRTOS_Workshop/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
